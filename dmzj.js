4d4040a9577cc68b28c5ec479ac4a5481cb5ddc3
flyingtassel <flyingtassel@163.com>
2016-01-08 23:34:42 +0800

    test-failed 160107
    dmzj的部分不work

---
+++ dmzj.js
@@ -0,0 +1,31 @@
+< script type = "text/javascript" >
+  var arr_img = new Array();
+var page = '';
+eval(function(p, a, c, k, e, d) {
+  e = function(c) {
+    return (c < a ? '' : e(parseInt(c / a))) + ((c = c % a) > 35 ? String
+      .fromCharCode(c + 29) : c.toString(36))
+  };
+  if (!''.replace(/^/, String)) {
+    while (c--) {
+      d[e(c)] = k[c] || e(c)
+    }
+    k = [function(e) {
+      return d[e]
+    }];
+    e = function() {
+      return '\\w+'
+    };
+    c = 1
+  };
+  while (c--) {
+    if (k[c]) {
+      p = p.replace(new RegExp('\\b' + e(c) + '\\b', 'g'), k[c])
+    }
+  }
+  return p
+}(
+  'y 8=8=\'{"z":"A","x":"w","s":"\\9\\7","t":"u","v":"B","C":I,"J":"K\\/5\\/3\\/2\\/H.4\\r\\6\\/5\\/3\\/2\\/G.4\\r\\6\\/5\\/3\\/2\\/D.4\\r\\6\\/5\\/3\\/2\\/q.4\\r\\6\\/5\\/3\\/2\\/F.4\\r\\6\\/5\\/3\\/2\\/L.4\\r\\6\\/5\\/3\\/2\\/o.4\\r\\6\\/5\\/3\\/2\\/a.4\\r\\6\\/5\\/3\\/2\\/g.4\\r\\6\\/5\\/3\\/2\\/e.4\\r\\6\\/5\\/3\\/2\\/d.4\\r\\6\\/5\\/3\\/2\\/c.4\\r\\6\\/5\\/3\\/2\\/b.4\\r\\6\\/5\\/3\\/2\\/f.4\\r\\6\\/5\\/3\\/2\\/p.4\\r\\6\\/5\\/3\\/2\\/n.4\\r\\6\\/5\\/3\\/2\\/i.4\\r\\6\\/5\\/3\\/2\\/l.4","k":"0","j":"0","m":"1","E":"T","M":"1f","1g":"18","1d":"1","1c":"19","1a":"\\1b\\1i\\1j\\1p\\1n","1k":"","1l":"","1m":"","1o":"","1h":"","16":"0","17":"0","U":"V","S":"0","R":"18","N":"","O":"","P":"","Q":"\\W\\X-13","14":"h","15":"12","11":"\\9\\7","Y":"\\Z\\10\\1e"}\';',
+  62, 88,
+  '||28747|536|jpg|chapterpic|nimg|u8bdd|pages|u7b2c35|1452170335328|14521703413156|14521703400209|14521703390824|14521703377881|14521703428097|14521703368087||14521703458815|chaptertype|chapter_type|14521703466705|chapter_true_type|14521703444112|14521703339945|14521703436879|14521703306939||chapter_name|chapter_order|44|createtime|16382|comic_id|var|id|49380|1452170369|folder|14521703298285|chapter_num|14521703319297|14521703290592|14521703288247|null|page_url|img|14521703331441|updatetime|keywords|comic_alias|comic_real|comic_name|picnum|high_file_size|35|filesize|1882419|u6d77|u72fcU|status|u8fde|u8f7d|last_update_chapter_name|hailangu37|37|first_letter|comic_py|hidden|direction||100099224|username|u6076|uid|sns_tag|u4e2d|1452217044|sum_pages|download|u4eba|u8c37|translatorid|translator|link|u753b|message|u6f2b'
+  .split('|'), 0, {})) < /script>
